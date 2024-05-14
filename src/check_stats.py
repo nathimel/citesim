d12f120eef550d2fd57af2a2d914e6b4485cffe8
Nathaniel Imel <nathanielimel@Nathaniels-MacBook-Air.local>
2024-05-13 23:25:57 -0700

    automate stat analysis

---
+++ src/check_stats.py
@@ -0,0 +1,83 @@
+"""Simple script to parse the .txt files containing results of regressions, and checks/reports the variance explained by field and density_bin, and the BIC difference between ablated model. """
+
+import os
+import pandas as pd
+from typing import Callable
+
+ANOVA_RESULTS_DIR = "./analysis_data/stats/anova_ablation"
+MODEL_SUMMARY_DIR = "./analysis_data/stats/summary"
+
+def tokenize(lines: list[str]) -> list[list[str]]:
+    return [line.split() for line in lines]
+
+def process_anova(data: list[list[str]]) -> tuple[float]:
+    model_ablated_bic = None
+    model_w_rho = None
+    
+    for tokens in data:
+        if tokens[0] == "model_ablated":
+            model_ablated_bic = tokens[3]
+        elif tokens[0] == "model_w_rho":
+            model_w_rho = tokens[3]
+
+    return (float(model_ablated_bic) - float(model_w_rho),)
+
+def process_summary(data: list[list[str]]) -> tuple[float]:
+
+    for num, tokens in enumerate(data):
+        if tokens[:2] == ["Random","effects:"]:
+            # Parse the lines containing effects variance explained
+            lines = data[num+2:num+5]
+            field_var = float(lines[0][2])
+            rho_var = float(lines[1][1])
+            res_var = float(lines[2][1])
+
+            total = field_var + rho_var + res_var
+            field_var_perc = (field_var / total) * 100
+            rho_var_perc = (rho_var / total) * 100
+            res_var = (res_var / total) * 100
+            break
+    
+    return (field_var_perc, rho_var_perc)
+
+    
+def process(mode: str) -> list[str]:
+
+    if mode == "anova":
+        dir = ANOVA_RESULTS_DIR
+        process_func = process_anova
+        cols = ["bic_gain"]
+    elif mode == "summary":
+        dir = MODEL_SUMMARY_DIR
+        process_func = process_summary
+        cols = ["field_var_percent", "density_var_percent"]
+
+    data = []
+    for filename in os.listdir(dir):
+        vectorizer = filename.split("_")[-1].split(".txt")[0]
+
+        with open(os.path.join(dir, filename), "r") as f:
+            lines = f.readlines()
+
+        tokens = tokenize(lines)
+        data.append(
+            (vectorizer, *process_func(tokens))
+        )
+    stat_df = pd.DataFrame(data, columns=["vectorizer"] + cols)
+    return stat_df
+
+
+if __name__ == "__main__":
+
+    # Anova
+    df = process("anova")
+    print("---------------ANOVA---------------")
+    print(df)
+    print()
+
+    # Summary
+    df = process("summary")
+    print("---------------SUMMARY---------------")
+    print(df)
+    print()
+    
--- src/regressions.R
+++ src/regressions.R
@@ -21,47 +21,74 @@ df_analysis <- read_csv("analysis_data/transformed_all_data.csv")
 
 
 # Regression
-getrefs <- function(y, vectorizer_name, print_summary=TRUE) {
+getrefs <- function(y, vectorizer_name, print_anova=TRUE) {
   
   df_vectorizer <- df_analysis %>% filter(
     vectorizer == vectorizer_name
   ) %>% mutate(
     # z-scale
     y = .data[[y]],
-    year_med_z = scale(year_med),
-    ref_med_z = scale(ref_med),
   ) %>% filter(
     ref_med_z < 3,
     ref_med_z > -3,
   )
   
-  # Model 5: 
-  label_5 <- paste(
-    # "y_z ~ year_med_z + density_bin_z + (1 | field)"
-    # "y_z ~ ref_med_z + year_med_z + density_bin_z + (1 | field)"
-    # "y_z ~ ref_med_z + year_med_z + density_bin_z + (1 + density_bin_z + ref_med_z | field)"
-    "y ~ ref_med_z + year_med_z + density_bin_z + (1 + density_bin_z | field)"
-  )
-  fm_5 <- as.formula(label_5)
-  model_5 <- lmer(
-    fm_5, 
+
+  model_w_rho <- lmer(
+    y ~ ref_med_z + year_med_z + density_bin_z + (1 + density_bin_z | field),
     data=df_vectorizer, 
     control=lmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5))
   )
   
-  if (print_summary) {
-    print(summary(model_5))    
-  }
+  # if (print_summary) {
+  #   print(summary(model_5))
+  # }
   
-  # model_ref_rho <- lmer(
-  #   ref_med_z ~ density_bin_z + (1 + density_bin_z | field), 
-  #   data=df_vectorizer,
-  #   control=lmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5))
-  # )
-  # 
-  # print(summary(model_ref_rho))
+  model_ablated <- lmer(
+    y ~ ref_med_z + year_med_z + (1 | field), 
+    data=df_vectorizer,
+    control=lmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5))
+  )
 
-  return(model_5)
+  stats_dir <- "analysis_data/stats/"
+  save_fn <- paste(
+    y, 
+    "_", 
+    vectorizer_name, 
+    ".txt", 
+    sep = ""
+  )
+  summary_save_fn <- paste(
+    stats_dir,
+    "summary/",
+    save_fn,
+    sep = ""
+  )
+  capture.output(
+    summary(model_w_rho),
+    file = summary_save_fn
+  ) 
+
+  # fc<-file(summary_save_fn)
+  # writeLines(c("--------------------------------------------------------------"), fc)
+  # close(fc)
+
+  anova_save_fn <- paste(
+    stats_dir,
+    "anova_ablation/",
+    save_fn,
+    sep = ""
+  )
+  capture.output(
+    anova(
+      model_w_rho,
+      model_ablated
+    ),
+    file = anova_save_fn
+  )
+  print(paste("Wrote to", anova_save_fn))
+
+  return(model_w_rho)
   
 }
 
@@ -87,14 +114,17 @@ for (y in c("cpy_med", "log_cpy_var")) {
       # plot.distributions = TRUE
     )
     + theme(
-      axis.text.x = element_text(size=20)
+      axis.text.x = element_text(size=20),
+      axis.text.y = element_blank()
     )
 
   )
   save_fn <- paste("analysis_data/figures/coefficients/", y, ".png", sep = "")
 
   ggsave(
-    save_fn
+    save_fn,
+    width = 6,
+    height = 4,
   )
   print(paste("Saved an image to", save_fn))
   
@@ -122,25 +152,35 @@ for (vec in unique(df_analysis$vectorizer)) {
                 color = field,
             ),
             size = 0.5, 
-            alpha = 0.2, 
+            alpha = 0.3, 
         )
         + geom_smooth(
             aes(
                 color = field,
             ),
             linewidth = 1,
+            level = 0.8,
         )
+
+        + scale_x_continuous(
+          limits = c(2008, 2016), 
+          breaks = seq(2008, 2016, by = 4),
+        )
+        # + ylim(-3,3)
         + theme_classic()
-        + ggtitle(vec)
+        # + ggtitle(vec)
         + theme(
-            axis.text = element_text(size=20)
+            axis.text = element_text(size=30),
+            axis.title = element_blank(),
         )
     )
 
   save_fn <- paste("analysis_data/figures/time/", vec, ".png", sep = "")
 
   ggsave(
-    save_fn
+    save_fn,
+    width = 8,
+    height = 5,
   )
   print(paste("Saved an image to", save_fn))
 
@@ -150,10 +190,7 @@ for (vec in unique(df_analysis$vectorizer)) {
 for (vec in unique(df_analysis$vectorizer)) {
     df_vec <- df_analysis %>% filter(
         vectorizer == vec
-    ) %>% mutate(
-    year_med_z = scale(year_med),
-    ref_med_z = scale(ref_med),
-  ) %>% filter(
+    ) %>% filter(
     ref_med_z < 3,
     ref_med_z > -3,
     )
@@ -162,8 +199,8 @@ for (vec in unique(df_analysis$vectorizer)) {
         ggplot(
             df_vec,
             aes(
-                x=density_bin_z,
-                y=ref_med_z,
+                x=density_bin,
+                y=ref_med,
             )
         )
         + geom_smooth(
@@ -176,25 +213,29 @@ for (vec in unique(df_analysis$vectorizer)) {
                 color = field,
             ),
             size = 0.5, 
-            alpha = 0.2, 
+            alpha = 0.3, 
         )
         + geom_smooth(
             aes(
                 color = field,
             ),
             linewidth = 1,
+            level = 0.8,
         )
         + theme_classic()
-        + ggtitle(vec)
+        # + ggtitle(vec)
         + theme(
-            axis.text = element_text(size=20)
+            axis.text = element_text(size=30),
+            axis.title = element_blank(),
         )
     )
 
   save_fn <- paste("analysis_data/figures/refs/", vec, ".png", sep = "")
 
   ggsave(
-    save_fn
+    save_fn,
+    width = 8,
+    height = 5,
   )
   print(paste("Saved an image to", save_fn))
 
